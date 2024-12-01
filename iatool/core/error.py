c946fc529c2bba8e559bdc479a4e53cb5a69657c
mmrakib <mohammadmrakib@gmail.com>
2024-12-02 09:28:15 +1100

    Implemented request handling with aiohttp

---
+++ iatool/core/error.py
@@ -0,0 +1,12 @@
+def format_error_message(type: str, message: str) -> str:
+    return f"{type}: {message}"
+
+class APIError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        self.error_message = format_error_message("APIError", message)
+
+        super().__init__(self.error_message)
+
+    def __str__(self) -> str:
+        return self.error_message
