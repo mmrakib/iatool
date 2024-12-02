c7f8d7cb733378a7bf81e23341ed97e8cdbde577
mmrakib <mohammadmrakib@gmail.com>
2024-12-02 17:10:17 +1100

    Renamed data fetch to specify FMP for future addition of multiple data sources

--- iatool/core/data.py
+++ iatool/core/data.py
@@ -6,16 +6,16 @@ import aiohttp
 from .config import Config
 from .error import APIError
 
-async def fetch_data(session: aiohttp.ClientSession, url: str, args: List[str] = []) -> List[str]:
+async def fetch_data_fmp(session: aiohttp.ClientSession, url: str, args: List[str] = []) -> List[str]:
     config = Config()
 
     args_str = "&".join(args)
-    full_url = f"{config.api.base_url}{url}?apikey={config.api.key}&{args_str}"
+    full_url = f"{config.api.fmp.base_url}{url}?apikey={config.api.fmp.key}&{args_str}"
 
     while True:
         try:
             async with session.get(full_url) as response:
-                if response.status == 429: # Too many requests
+                if response.status == 429:  # Too many requests
                     await asyncio.sleep(config.api.retry_delay)
                 response.raise_for_status()
                 return await response.json()
--- tests/manual/data/test_fetch_data.py
+++ tests/manual/data/test_fetch_data.py
@@ -2,12 +2,12 @@ import asyncio
 
 import aiohttp
 
-from iatool.core.data import fetch_data
+from iatool.core.data import fetch_data_fmp
 
 async def main():
     async with aiohttp.ClientSession() as session:
         url = "/profile/AAPL"
-        data = await fetch_data(session, url)
+        data = await fetch_data_fmp(session, url)
 
         print(data)
 
